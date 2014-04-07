fda58127616329bc38fb0b188c10bbfd7b19a0a7
Lily Hoskin <lilyhoskin@gmail.com>
2014-04-07 22:57:22 +0100

    Problems 10 to 20

--- Lists.hs
+++ Lists.hs
@@ -54,3 +54,54 @@ pack l  = let duplicates = countDuplicatesAtStart l in (take duplicates l) : pac
 encode :: Eq a => [a] -> [(Int, a)]
 encode [] = []
 encode l = map (\l -> (length l, head l)) (pack l)
+
+-- Problem 11
+data OneOrMany a = One a | Many (Int, a) deriving (Show)
+encodeModified :: Eq a => [a] -> [OneOrMany a]
+encodeModified [] = []
+encodeModified l = map (\x -> if fst x == 1 then One (snd(x)) else Many x) (encode l)
+
+-- Problem 12
+decodeModified :: [OneOrMany a] -> [a]
+decodeModified [] = []
+decodeModified (x:xs) = case x of One x      -> x : decodeModified xs
+                                  Many (n,x) -> take n (repeat x) ++ decodeModified xs
+
+-- Problem 13
+encodeDirect :: Eq a => [a] -> [OneOrMany a]
+encodeDirect [] = []
+encodeDirect l@(x:xs) = let duplicates = countDuplicatesAtStart l in
+                        (if duplicates == 1 then One x else Many (duplicates, x)) : encodeDirect (drop duplicates l)
+
+-- Problem 14
+dupli :: [a] -> [a]
+dupli l = concatMap (\x -> [x,x]) l
+
+-- Problem 15
+repli :: [a] -> Int -> [a]
+repli l n = concatMap (\x -> n `take` repeat x) l
+
+-- Problem 16
+dropEvery :: [a] -> Int -> [a]
+dropEvery l n = map snd (filter (\x -> fst x `mod` n /= 0) (zip [1..] l))
+
+-- Problem 17
+split :: [a] -> Int -> ([a], [a])
+split l 0 = ([], l)
+split (x:xs) n = let splitTail = split (xs) (n-1) in ((x: fst splitTail), (snd splitTail))
+
+-- Problem 18
+slice :: [a] -> Int -> Int -> [a]
+slice l i k = take (k-i-1) (drop (i-1) l)
+
+-- Problem 19
+rotate :: [a] -> Int -> [a]
+rotate l n = take (length l) (drop n (cycle l))
+
+-- Problem 20
+removeAt :: Int -> [a] -> [a]
+removeAt 1 l = tail l
+removeAt n l
+  | n <= 0    = error("Element to remove must be greater than 0")
+  | null l    = error("Index out of bounds")
+  | otherwise = head l : removeAt (n-1) (tail l)
